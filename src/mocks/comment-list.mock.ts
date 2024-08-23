4eca33050a8acc900dd59ecb92b43a3b50ecfb64
postwo <codo7717@naver.com>
2024-08-23 14:23:29 +0900

    댓글 리스트 아이템

---
+++ src/mocks/comment-list.mock.ts
@@ -0,0 +1,28 @@
+import { CommentListItem } from "../types/interface";
+
+
+const commentListMock: CommentListItem [] = [
+    
+        {
+            "nickname" : "안녕하세요",
+            "profileImage" : null,
+            "writeDatetime" : "3분전",
+            "content" : "오늘 저는 리액트를 공부중이에요"
+        },
+        {
+            "nickname" : "안녕하세요",
+            "profileImage" : null,
+            "writeDatetime" : "3분전",
+            "content" : "오늘 저는 리액트를 공부중이에요"
+        },
+        {
+            "nickname" : "안녕하세요",
+            "profileImage" : null,
+            "writeDatetime" : "3분전",
+            "content" : "오늘 저는 리액트를 공부중이에요"
+        }
+    
+];
+
+
+export default commentListMock
