4e11b59bf6be5a0f68267747e75daa9cfbc74b8e
postwo <codo7717@naver.com>
2024-08-26 17:59:14 +0900

    Footer 레이아웃

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import React, { useState } from 'react';
 import './App.css';
-import Top3Item from './components/Top3Item';
-import top3boardListMock from './mocks/top-3-board-list.mock';
-import CommentItem from './components/CommentItem';
-import { pseudoRandomBytes } from 'crypto';
-import commentListMock from './mocks/comment-list.mock';
-import favoriteListMock from './mocks/favorite-list.mock';
-import FavoriteItem from './components/FavoriteItem';
-import InputBox from './components/InputBox';
+import Footer from './layouts/Footer';
+
 
 // return 에 <BoardListItem/> 이렇게 넣어야 내가 만든 div들이 출력된다
 function App() {
-  
-  const [value,setValue] =useState<string>('');
 
   return (
     <>
-      <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={true} setValue={setValue} message='aaaa'/>
+      <Footer />
     </>
   );
 }
